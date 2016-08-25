675cab4ff4a148ae67a1f9644989e2c58c6dd72f
caobotao <958735390@qq.com>
2016-08-25 19:56:46 +0800

    创建包结构及商品实体类

---
+++ src/com/cbt/domain/Product.java
@@ -0,0 +1,30 @@
+package com.cbt.domain;
+
+/**
+ * 商品实体类
+ * @author caobotao
+ */
+public class Product {
+	private Integer pid;
+	private String pname;
+	private Double price;
+	public Integer getPid() {
+		return pid;
+	}
+	public void setPid(Integer pid) {
+		this.pid = pid;
+	}
+	public String getPname() {
+		return pname;
+	}
+	public void setPname(String pname) {
+		this.pname = pname;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+}
